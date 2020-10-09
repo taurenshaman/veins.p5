4e83c822265447e75b4db2dd750c2634af0c3dd8
Jerin Lee <taurenjerin@qq.com>
2020-10-09 13:41:52 +0800

    update getRandomInt.
    Signed-off-by: Jerin Lee <taurenjerin@qq.com>

--- src/tools/CommonUtility.ts
+++ src/tools/CommonUtility.ts
@@ -18,8 +18,38 @@ class CommonUtitlity {
      * return a integer number less than max
      * @param max 
      */
-    static getRandomInt(max: number) {
-        return Math.floor(Math.random() * Math.floor(max));
+    static getRandomInt(max: number, min = 0) {
+        //return Math.floor(Math.random() * Math.floor(max));
+
+        // https://stackoverflow.com/a/41452318/1247872
+        let range = max - min;
+        if (range <= 0) {
+            //throw ('max must be larger than min');
+            const tmp = max;
+            max = min;
+            min = tmp;
+            range = -range;
+        }
+        
+        var requestBytes = Math.ceil(Math.log2(range) / 8);
+        if (!requestBytes) { // No randomness required
+            return min;
+        }
+        var maxNum = Math.pow(256, requestBytes);
+        var ar = new Uint8Array(requestBytes);
+    
+        while (true) {
+            window.crypto.getRandomValues(ar);
+    
+            var val = 0;
+            for (var i = 0;i < requestBytes;i++) {
+                val = (val << 8) + ar[i];
+            }
+    
+            if (val < maxNum - maxNum % range) {
+                return min + (val % range);
+            }
+        }
     }
 
     static getRandomElement(list) {
